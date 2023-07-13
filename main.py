c345c13f9a894c0de1895fbecf6f4228d3861d67
Humberto Moncada <humberto@bitegra.mx>
2023-07-13 13:46:13 -0600

    Changes in main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+# from flask_parameter_validation import ValidateParameters, Route, Json, Query
+# from datetime import datetime
 
 app = Flask(__name__)
+
 
 
 @app.route("/")
@@ -8,10 +11,14 @@ def hello():
     return "<h2>Para empezar</h2>"
 
 
-@app.route("/<string:name>/")
-def say_hello(name):
-    return f"What's so matter {name}!"
-
+# @app.route("/<string:name>/")
+# def say_hello(name):
+#     return f"What's so matter {name}!"
+@app.route("/login")
+@app.route("/login/<variable>")
+# @ValidateParameters()
+def login(variable=None):
+    return render_template("index.html", name=variable)
 
 if __name__ == "__main__":
     app.run()
